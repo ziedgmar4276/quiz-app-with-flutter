2681507cd3ed25514c1723c8ce0113d9f7ce412a
ziedgmar <ziedgmar242@gmail.com>
2020-08-25 00:01:02 +0100

    condition question

--- lib/main.dart
+++ lib/main.dart
@@ -13,37 +13,48 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = const[
+    {
+      'questionText': 'what is  you favorite color?',
+      'answers': ['Black', 'blue', 'white', 'red']
+    },
+    {
+      'questionText': 'what is  your favorite  animal  ?',
+      'answers': ['chat', 'elephone', 'snake', 'lion']
+    },
+    {
+      'questionText': 'what is  your favorite  introduction ?',
+      'answers': ['zied', 'zied', 'snake', 'lion']
+    },
+  ];
   var _questionIndex = 0;
 
   void _answarQuestions() {
-    setState(() {
-      _questionIndex = _questionIndex + 1;
-    });
-    print(_questionIndex);
+
+      setState(() {
+        _questionIndex = _questionIndex + 1;
+      });
+      print(_questionIndex);
+      if(_questionIndex< questions.length )
+        {
+          print('you have more question');
+        }else
+          {
+            print('no more question');
+          }
+
+
   }
 
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        'questionText': 'what is  you favorite color?',
-        'answers': ['Black', 'blue', 'white', 'red']
-      },
-      {
-        'questionText': 'what is  your favorite  animal  ?',
-        'answers': ['chat', 'elephone', 'snake', 'lion']
-      },
-      {
-        'questionText': 'what is  your favorite  introduction ?',
-        'answers': ['zied', 'zied', 'snake', 'lion']
-      },
-    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('QUESTION ANSWER'),
         ),
-        body: Column(
+        body:_questionIndex< questions.length ? Column(
             children: [
           Question(
             questions[_questionIndex]['questionText'],
@@ -53,7 +64,7 @@ class _MyAppState extends State<MyApp> {
            return Answer(_answarQuestions,answer);
          }).toList()
         ],
-        ),
+        ):Center(child :Text('You did it!') ),
       ),
     );
   }
